ffdb3a181b9cb1a14190057e9c564f4115bbc551
nullorone <sy.v@mail.ru>
2019-11-03 12:44:20 +0300

    Добавляет компонент Title

---
+++ src/components/title/title.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const {string} = PropTypes;
+
+const Title = (props) => {
+  const {className, text} = props;
+
+  return (<h1 className={className}>{text}</h1>);
+}
+
+Title.propTypes = {
+  className: string.isRequired,
+  text: string.isRequired,
+};
+
+export default Title;
