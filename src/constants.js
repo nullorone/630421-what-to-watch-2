040efe3258cd1bc99040239e4c886b65bc1cb7da
nullorone <sy.v@mail.ru>
2019-11-03 12:50:05 +0300

    Добавляет константы

--- src/constants.js
+++ src/constants.js
@@ -5,3 +5,61 @@ export const Value = {
   FULL: 1,
 };
 
+export const Pathname = {
+  DEFAULT: `/`,
+  DETAILS: `/details`,
+};
+
+export const Img = {
+  SMALL: {
+    width: `280`,
+    height: `175`
+  },
+  BIG: {
+    width: `218`,
+    height: `327`
+  }
+};
+
+export const iconNames = [`add`, `fullscreen`, `in_list`, `pause`];
+
+export const MOVIE_CARD_BUTTONS = [
+  {
+    iconName: `play-s`,
+    classModifier: `play`,
+    text: `Play`,
+  },
+  {
+    iconName: `add`,
+    classModifier: `list`,
+    text: `My list`,
+  }
+];
+
+export const COPYRIGHT = `© 2019 What to watch Ltd.`;
+
+export const CINEMA_NAME = `WTW`;
+
+export const MOVIE_NAV_ITEMS = [
+  {
+    active: false,
+    text: `Overview`,
+  },
+  {
+    active: true,
+    text: `Details`,
+  },
+  {
+    active: false,
+    text: `Reviews`,
+  },
+];
+
+export const TypeCol = {
+  TEXT: `text`,
+  REVIEWS: `reviews`,
+};
+
+export const AMOUNT_SIMILAR_FILMS = 4;
+
+export const MINUTES_IN_HOUR = 60;
