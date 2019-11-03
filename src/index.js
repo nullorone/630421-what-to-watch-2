d61627a4633d744eed0128d8a1fa81e45db5a8e9
nullorone <sy.v@mail.ru>
2019-11-03 12:51:04 +0300

    Добавляет массив используемых иконок в изначальные данные

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './components/app/app';
 import {films} from './mocks/films';
 import {genres} from './mocks/genres';
+import {iconNames} from "./constants";
 
 const initData = {
   films,
   genres,
+  iconNames,
 };
 
 const init = (initialData) => {
