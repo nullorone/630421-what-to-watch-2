76a0768daf84d2bc43651acf07b833d613f02eee
nullorone <sy.v@mail.ru>
2019-11-03 12:51:39 +0300

    Заменяет разметку на компоненты

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -2,6 +2,17 @@ import React, {PureComponent} from "react";
 import PropTypes from 'prop-types';
 import MovieCardSmallList from "../movie-card-small-list/movie-card-small-list";
 import GenreList from "../genre-list/genre-list";
+import IconsWrapper from "../icons-wrapper/icons-wrapper";
+import CatalogButton from "../catalog-button/catalog-button";
+import Copyright from "../copyright/copyright";
+import Logo from "../logo/logo";
+import UserBlock from "../user-block/user-block";
+import Header from "../header/header";
+import Title from "../title/title";
+import MovieCardPicture from "../movie-card-picture/movie-card-picture";
+import MovieCardButtonList from "../movie-card-button-list/movie-card-button-list";
+import {MOVIE_CARD_BUTTONS, COPYRIGHT, CINEMA_NAME} from "../../constants";
+import MovieCardDescription from "../movie-card-description/movie-card-description";
 
 const {string, shape, exact, arrayOf} = PropTypes;
 
@@ -14,105 +25,47 @@ export default class Main extends PureComponent {
     const {
       films,
       genres,
+      icons,
     } = this.props;
-
     return (
       <>
-        <div className="visually-hidden">
-          <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
-            <symbol id="add" viewBox="0 0 19 20">
-              <title>+</title>
-              <desc>Created with Sketch.</desc>
-              <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-                <polygon id="+" fill="#EEE5B5"
-                  points="10.777832 11.2880859 10.777832 19.5527344 8.41650391 19.5527344 8.41650391 11.2880859 0.627929688 11.2880859 0.627929688 8.92675781 8.41650391 8.92675781 8.41650391 0.662109375 10.777832 0.662109375 10.777832 8.92675781 18.5664062 8.92675781 18.5664062 11.2880859"/>
-              </g>
-            </symbol>
-            <symbol id="full-screen" viewBox="0 0 27 27">
-              <path fillRule="evenodd" clipRule="evenodd" d="M23.8571 0H16V3.14286H23.8571V11H27V3.14286V0H23.8571Z"
-                fill="#FFF9D9" fillOpacity="0.7"/>
-              <path fillRule="evenodd" clipRule="evenodd" d="M27 23.8571V16H23.8571V23.8571H16V27H23.8571H27L27 23.8571Z"
-                fill="#FFF9D9" fillOpacity="0.7"/>
-              <path fillRule="evenodd" clipRule="evenodd"
-                d="M0 3.14286L0 11H3.14286L3.14286 3.14286L11 3.14286V0H3.14286H0L0 3.14286Z" fill="#FFF9D9"
-                fillOpacity="0.7"/>
-              <path fillRule="evenodd" clipRule="evenodd"
-                d="M3.14286 27H11V23.8571H3.14286L3.14286 16H0L0 23.8571V27H3.14286Z" fill="#FFF9D9"
-                fillOpacity="0.7"/>
-            </symbol>
-            <symbol id="in-list" viewBox="0 0 18 14">
-              <path fillRule="evenodd" clipRule="evenodd"
-                d="M2.40513 5.35353L6.1818 8.90902L15.5807 0L18 2.80485L6.18935 14L0 8.17346L2.40513 5.35353Z"
-                fill="#EEE5B5"/>
-            </symbol>
-            <symbol id="pause" viewBox="0 0 14 21">
-              <title>Artboard</title>
-              <desc>Created with Sketch.</desc>
-              <g id="Artboard" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-                <polygon id="Line" fill="#EEE5B5" fillRule="nonzero"
-                  points="0 -1.11910481e-13 4 -1.11910481e-13 4 21 0 21"/>
-                <polygon id="Line" fill="#EEE5B5" fillRule="nonzero"
-                  points="10 -1.11910481e-13 14 -1.11910481e-13 14 21 10 21"/>
-              </g>
-            </symbol>
-          </svg>
-        </div>
+        {icons && <IconsWrapper iconNames={icons}/>}
+
         <section className="movie-card">
-          <div className="movie-card__bg">
-            <img src="img/bg-the-grand-budapest-hotel.jpg" alt="The Grand Budapest Hotel"/>
-          </div>
 
-          <h1 className="visually-hidden">WTW</h1>
+          {<MovieCardPicture
+            className={`movie-card__bg`}
+            picture={{
+              src: `./img/bg-the-grand-budapest-hotel.jpg`,
+              alt: `The Grand Budapest Hotel`,
+            }}/>}
 
-          <header className="page-header movie-card__head">
-            <div className="logo">
-              <a className="logo__link">
-                <span className="logo__letter logo__letter--1">W</span>
-                <span className="logo__letter logo__letter--2">T</span>
-                <span className="logo__letter logo__letter--3">W</span>
-              </a>
-            </div>
+          {<Title className={`visually-hidden`} text={CINEMA_NAME}/>}
 
-            <div className="user-block">
-              <div className="user-block__avatar">
-                <img src="img/avatar.jpg" alt="User avatar" width="63" height="63"/>
-              </div>
-            </div>
-          </header>
+          {<Header>
+            <Logo light={false}/>
+            <UserBlock avatarSrc={`./img/avatar.jpg`}/>
+          </Header>}
 
           <div className="movie-card__wrap">
             <div className="movie-card__info">
-              <div className="movie-card__poster">
-                <img
-                  src="img/the-grand-budapest-hotel-poster.jpg"
-                  alt="The Grand Budapest Hotel poster"
-                  width="218"
-                  height="327"
-                />
-              </div>
 
-              <div className="movie-card__desc">
-                <h2 className="movie-card__title">The Grand Budapest Hotel</h2>
-                <p className="movie-card__meta">
-                  <span className="movie-card__genre">Drama</span>
-                  <span className="movie-card__year">2014</span>
-                </p>
+              {<MovieCardPicture
+                className={`movie-card__poster`}
+                picture={{
+                  src: `./img/bg-the-grand-budapest-hotel.jpg`,
+                  alt: `The Grand Budapest Hotel`,
+                  width: `218`,
+                  height: `327`
+                }}/>}
 
-                <div className="movie-card__buttons">
-                  <button className="btn btn--play movie-card__button" type="button">
-                    <svg viewBox="0 0 19 19" width="19" height="19">
-                      <use xlinkHref="#play-s" />
-                    </svg>
-                    <span>Play</span>
-                  </button>
-                  <button className="btn btn--list movie-card__button" type="button">
-                    <svg viewBox="0 0 19 20" width="19" height="20">
-                      <use xlinkHref="#add" />
-                    </svg>
-                    <span>My list</span>
-                  </button>
-                </div>
-              </div>
+              {<MovieCardDescription
+                title={`The Grand Budapest Hotel`}
+                genre={`Drama`}
+                year={`2014`}
+              >
+                <MovieCardButtonList buttons={MOVIE_CARD_BUTTONS}/>
+              </MovieCardDescription>}
             </div>
           </div>
         </section>
@@ -127,23 +80,14 @@ export default class Main extends PureComponent {
 
             {films && <MovieCardSmallList films={films}/>}
 
-            <div className="catalog__more">
-              <button className="catalog__button" type="button">Show more</button>
-            </div>
+            {<CatalogButton
+              text={`ShowMore`}
+              onButtonClick={() => console.log(`click-clack`)}/>}
           </section>
 
           <footer className="page-footer">
-            <div className="logo">
-              <a className="logo__link logo__link--light">
-                <span className="logo__letter logo__letter--1">W</span>
-                <span className="logo__letter logo__letter--2">T</span>
-                <span className="logo__letter logo__letter--3">W</span>
-              </a>
-            </div>
-
-            <div className="copyright">
-              <p>© 2019 What to watch Ltd.</p>
-            </div>
+            {<Logo light={true}/>}
+            {<Copyright text={COPYRIGHT}/>}
           </footer>
         </div>
       </>
@@ -163,5 +107,6 @@ Main.propTypes = {
       })
   ),
   genres: arrayOf(string.isRequired),
+  icons: arrayOf(string.isRequired),
 };
 
