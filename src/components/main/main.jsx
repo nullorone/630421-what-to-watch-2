062d216e882acd284907765b8a1d8f348796d3fc
nullorone <sy.v@mail.ru>
2019-11-04 23:30:32 +0300

    Реализует добавление фильмов при нажатии на кнопку Show more

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -11,14 +11,29 @@ import Header from "../header/header";
 import Title from "../title/title";
 import MovieCardPicture from "../movie-card-picture/movie-card-picture";
 import MovieCardButtonList from "../movie-card-button-list/movie-card-button-list";
-import {MOVIE_CARD_BUTTONS, COPYRIGHT, CINEMA_NAME} from "../../constants";
+import {
+  MOVIE_CARD_BUTTONS,
+  COPYRIGHT,
+  CINEMA_NAME,
+  Img,
+  AmountSimilarFilms,
+  AMOUNT_ADDED_FILMS,
+  Value,
+} from "../../constants";
 import MovieCardDescription from "../movie-card-description/movie-card-description";
 
-const {string, shape, exact, arrayOf} = PropTypes;
+const {string, shape, number, bool, arrayOf} = PropTypes;
 
 export default class Main extends PureComponent {
   constructor(props) {
     super(props);
+
+    this._currentAmountSimilarFilm = AmountSimilarFilms.DEFAULT;
+
+    this.state = {
+      films: this.props.films.slice(Value.EMPTY, AmountSimilarFilms.DEFAULT),
+      hasCatalogButton: true,
+    };
   }
 
   render() {
@@ -27,6 +42,20 @@ export default class Main extends PureComponent {
       genres,
       icons,
     } = this.props;
+
+    const favoriteFilm = films.findIndex((film) => film.isFavorite);
+    const {
+      name,
+      image: {
+        poster,
+        posterAlt,
+        background,
+        backgroundAlt,
+      },
+      genre,
+      released,
+    } = films[favoriteFilm];
+
     return (
       <>
         {icons && <IconsWrapper iconNames={icons}/>}
@@ -36,8 +65,8 @@ export default class Main extends PureComponent {
           {<MovieCardPicture
             className={`movie-card__bg`}
             picture={{
-              src: `./img/bg-the-grand-budapest-hotel.jpg`,
-              alt: `The Grand Budapest Hotel`,
+              src: background,
+              alt: backgroundAlt,
             }}/>}
 
           {<Title className={`visually-hidden`} text={CINEMA_NAME}/>}
@@ -53,16 +82,16 @@ export default class Main extends PureComponent {
               {<MovieCardPicture
                 className={`movie-card__poster`}
                 picture={{
-                  src: `./img/bg-the-grand-budapest-hotel.jpg`,
-                  alt: `The Grand Budapest Hotel`,
-                  width: `218`,
-                  height: `327`
+                  src: poster,
+                  alt: posterAlt,
+                  width: Img.BIG.width,
+                  height: Img.BIG.height,
                 }}/>}
 
               {<MovieCardDescription
-                title={`The Grand Budapest Hotel`}
-                genre={`Drama`}
-                year={`2014`}
+                title={name}
+                genre={genre}
+                year={released}
               >
                 <MovieCardButtonList buttons={MOVIE_CARD_BUTTONS}/>
               </MovieCardDescription>}
@@ -78,11 +107,31 @@ export default class Main extends PureComponent {
 
             {genres && <GenreList genres={genres}/>}
 
-            {films && <MovieCardSmallList films={films}/>}
+            {this.state.films && <MovieCardSmallList films={this.state.films}/>}
 
-            {<CatalogButton
+            {this.state.hasCatalogButton && <CatalogButton
               text={`ShowMore`}
-              onButtonClick={() => console.log(`click-clack`)}/>}
+              onButtonClick={(evt) => {
+                evt.preventDefault();
+                this.setState((prevState) => {
+                  const startIndexFilm = this._currentAmountSimilarFilm;
+                  let hasCatalogButton = this.state.hasCatalogButton;
+                  let endIndexFilm = Value.EMPTY;
+
+                  if ((startIndexFilm + AMOUNT_ADDED_FILMS) > this.props.films.length) {
+                    hasCatalogButton = false;
+                    endIndexFilm = this.props.films.length;
+                  } else {
+                    endIndexFilm = startIndexFilm + AMOUNT_ADDED_FILMS;
+                    this._currentAmountSimilarFilm = endIndexFilm;
+                  }
+
+                  return {
+                    films: [...prevState.films, ...this.props.films.slice(startIndexFilm, endIndexFilm)],
+                    hasCatalogButton,
+                  };
+                });
+              }}/>}
           </section>
 
           <footer className="page-footer">
@@ -97,15 +146,31 @@ export default class Main extends PureComponent {
 
 
 Main.propTypes = {
-  films: arrayOf(
-      shape({
-        title: string.isRequired,
-        img: exact({
-          src: string.isRequired,
-          alt: string.isRequired
-        }),
-      })
-  ),
+  films: arrayOf(shape({
+    id: number.isRequired,
+    name: string.isRequired,
+    image: shape({
+      poster: string.isRequired,
+      posterAlt: string.isRequired,
+      preview: string.isRequired,
+      previewAlt: string.isRequired,
+      background: string.isRequired,
+    }),
+    backgroundColor: string.isRequired,
+    video: shape({
+      link: string.isRequired,
+      preview: string.isRequired,
+    }),
+    description: string.isRequired,
+    rating: number.isRequired,
+    scoresCount: number.isRequired,
+    director: string.isRequired,
+    starring: arrayOf(string.isRequired),
+    runTime: number.isRequired,
+    genre: string.isRequired,
+    released: number.isRequired,
+    isFavorite: bool.isRequired,
+  })),
   genres: arrayOf(string.isRequired),
   icons: arrayOf(string.isRequired),
 };
