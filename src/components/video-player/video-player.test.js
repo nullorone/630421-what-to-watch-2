e06c60b00d29d29bb7fab154cb48352b2fb84ec9
ysveshnikov <ysveshnikov@ivi.ru>
2019-11-07 10:42:40 +0300

    Добавляет тесты

---
+++ src/components/video-player/video-player.test.js
@@ -0,0 +1,38 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import VideoPlayer from "./video-player";
+
+describe(`Make snapshot`, () => {
+  const initProps = {
+    video: {
+      link: {
+        mp4: `New Year`,
+        webm: `Christmas`,
+      },
+      poster: `./img/Holly`,
+    },
+    isMuted: true,
+  };
+
+  const createNodeMock = (element) => {
+    return element.type === `video`
+      ? {
+        play: () => {},
+        pause: () => {},
+      }
+      : null;
+  };
+
+  it(`Get snapshot component`, () => {
+    const tree = renderer
+      .create(
+          <VideoPlayer
+            video={initProps.video}
+          />,
+          {createNodeMock}
+      )
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
