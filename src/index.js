4c57b97629e6e17cb1572e6cfdf774ffecffb71f
nullorone <sy.v@mail.ru>
2019-11-09 20:04:39 +0300

    Формирует массив жанров на основе моковых фильмов

--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './components/app/app';
 import {films} from './mocks/films';
 import {promo} from "./mocks/promo";
-import {genres} from './mocks/genres';
 import {iconNames} from "./constants";
+
+const getGenres = (filmsOnPage) => {
+  return new Set(filmsOnPage.map((film) => film.genre));
+};
+
+const genres = [`All genres`, ...getGenres(films)];
 
 const initData = {
   promo,
