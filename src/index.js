a0673255107db9b4d53a6c7d9ba280d7a71ed0bd
nullorone <sy.v@mail.ru>
2019-11-16 19:39:24 +0300

    Подключает Provider

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import App from './components/app/app';
 import {films} from './mocks/films';
 import {promo} from "./mocks/promo";
 import {iconNames} from "./constants";
+import {Provider} from "react-redux";
+import {createStore} from "redux";
+import {reducer} from "./reducer";
 
 const getGenres = (filmsOnPage) => {
   return new Set(filmsOnPage.map((film) => film.genre));
@@ -19,10 +22,20 @@ const initData = {
 };
 
 const init = (initialData) => {
+  /* eslint-disable no-underscore-dangle */
+  const store = createStore(
+      reducer,
+      window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+  );
+  /* eslint-enable */
+
   ReactDOM.render(
-      <App
-        {...initialData}
-      />,
+      <Provider store={store}>
+        <App
+          {...initialData}
+        />
+      </Provider>
+      ,
       document.querySelector(`#root`)
   );
 };
