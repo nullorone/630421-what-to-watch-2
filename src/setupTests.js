4ec41f23f5b2cf861d8afbc03e210ac6b5e4b2b7
nullorone <sy.v@mail.ru>
2019-11-25 22:15:36 +0300

    Переносит настройки

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,3 +2,19 @@ import {configure} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({adapter: new Adapter()});
+
+module.exports = {
+  transform: {
+    "^.+\\.tsx?$": `ts-jest`,
+    "^.+\\.js?$": `babel-jest`,
+  },
+  testRegex: `.test.(js?|jsx?|tsx?)$`,
+  moduleFileExtensions: [
+    `ts`,
+    `tsx`,
+    `js`,
+    `jsx`,
+    `json`,
+    `node`
+  ],
+};
