4dfd9e47ce6f7a98308afb92e4ea999ca6ad596a
nullorone <sy.v@mail.ru>
2019-11-30 21:13:22 +0300

    Подключает апи к стору

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,23 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './components/app/app';
 import {Provider} from "react-redux";
-import {createStore} from "redux";
+import {createStore, applyMiddleware} from "redux";
 import {reducer} from "./reducer";
+import thunk from "redux-thunk";
+import {compose} from "recompose";
+import createApi from "./api";
 
 declare const __REDUX_DEVTOOLS_EXTENSION__: () => any;
 
-const init = () => {
+const init = (): void => {
+  const api = createApi((...args) => store.dispatch(...args));
   /* eslint-disable no-underscore-dangle */
   const store = createStore(
       reducer,
-      __REDUX_DEVTOOLS_EXTENSION__ && __REDUX_DEVTOOLS_EXTENSION__()
+      compose(
+          applyMiddleware(thunk.withExtraArgument(api)),
+          __REDUX_DEVTOOLS_EXTENSION__ && __REDUX_DEVTOOLS_EXTENSION__()
+      ),
   );
   /* eslint-enable */
 
