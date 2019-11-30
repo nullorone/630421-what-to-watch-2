302daacb11d9f3184a05a6979ddec30269719cb1
nullorone <sy.v@mail.ru>
2019-11-30 14:11:36 +0300

    Добавляет иконку play

--- src/icons.js
+++ src/icons.js
@@ -34,14 +34,17 @@ export const IconSymbol = {
   ),
   PAUSE: (
     <symbol id="pause" viewBox="0 0 14 21">
-      <title>Artboard</title>
-      <desc>Created with Sketch.</desc>
       <g id="Artboard" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
         <polygon id="Line" fill="#EEE5B5" fillRule="nonzero"
           points="0 -1.11910481e-13 4 -1.11910481e-13 4 21 0 21"/>
         <polygon id="Line" fill="#EEE5B5" fillRule="nonzero"
           points="10 -1.11910481e-13 14 -1.11910481e-13 14 21 10 21"/>
       </g>
+    </symbol>
+  ),
+  PLAY: (
+    <symbol id="play-s" viewBox="0 0 19 19">
+      <path fillRule="evenodd" clipRule="evenodd" d="M0 0L19 9.5L0 19V0Z" fill="#EEE5B5"/>
     </symbol>
   ),
 };
