1c1855bb934a92d02162c12de847bf08881d04af
nullorone <sy.v@mail.ru>
2019-11-30 15:56:02 +0300

    Обновляет конфиги

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,37 @@
-const path = require(`path`);
+const path = require('path');
 
 module.exports = {
-  entry: `./src/index.tsx`,
+  entry: './src/index.tsx',
   output: {
-    filename: `bundle.js`,
-    path: path.join(__dirname, `public`),
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'public')
   },
   devServer: {
-    contentBase: path.join(__dirname, `public`),
+    contentBase: path.join(__dirname, 'public'),
     compress: false,
-    open: true,
-    port: 8080,
+    port: 1337,
     historyApiFallback: true,
   },
   module: {
     rules: [
       {
-        test: /\.(js|jsx|ts|tsx)$/,
+        test: /\.(ts|tsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: `babel-loader`,
+          loader: 'ts-loader',
         },
-      }
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
     ],
   },
-  devtool: `source-map`,
+  devtool: 'source-map',
   resolve: {
-    extensions: [`.js`, `.jsx`, `.ts`, `.tsx`]
-  }
+    extensions: ['.ts', '.tsx', '.js', '.jsx', 'json']
+  },
 };
