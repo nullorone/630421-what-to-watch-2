e3ef0fda8f6cccf0671356a2fdcf2fbf1213e6b7
nullorone <sy.v@mail.ru>
2019-12-10 21:12:50 +0300

    Исправляет ссылки

--- src/api.js
+++ src/api.js
@@ -1,6 +1,8 @@
+import React from "react";
 import axios from "axios";
 import {API_TIMEOUT, Status, Url} from "./constants";
 import {ActionCreator} from "./reducer/data/data";
+import {Redirect} from "react-router-dom";
 
 const createApi = (dispatch) => {
   const api = axios.create({
@@ -14,6 +16,7 @@ const createApi = (dispatch) => {
   const onFail = (error) => {
     if (error.response.status === Status.AUTHORIZATION) {
       dispatch(ActionCreator.requireAuthorization(true));
+      return <Redirect to="/login"/>;
     }
 
     return error;
